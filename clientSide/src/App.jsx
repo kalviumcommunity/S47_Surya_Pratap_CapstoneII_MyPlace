30854ef6230d94fc5c0e0cb286b6696b0ec2d16d
Surya <thesuryapratap2003@gmail.com>
2024-04-13 14:02:39 +0530

    Created-Few-Components

--- clientSide/src/App.jsx
+++ clientSide/src/App.jsx
@@ -1,3 +1,9 @@
+import { Route, Routes } from "react-router-dom"
+import Home from "./components/Home"
+import SignIn from "./components/SignIn"
+import SignUp from "./components/SignUp"
+import About from "./components/About"
+import Profile from "./components/Profile"
 
 function App() {
 
@@ -6,6 +12,13 @@ function App() {
       <h1 className="text-red-500">
         Hello world!
       </h1>
+      <Routes>
+        <Route path="/" element={<Home/>} ></Route>
+        <Route path="/sign-in" element={<SignIn/>} ></Route>
+        <Route path="/sign-Up" element={<SignUp/>} ></Route>
+        <Route path="/about" element={<About/>} ></Route>
+        <Route path="/profile" element={<Profile/>} ></Route>
+      </Routes>
     </>
   )
 }
