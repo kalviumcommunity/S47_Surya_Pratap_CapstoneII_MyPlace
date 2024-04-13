32ad1b1db0d970b57a68aa3d7147ef0f4095b75b
Surya <thesuryapratap2003@gmail.com>
2024-04-13 15:47:22 +0530

    Created-a-header-component

--- clientSide/src/App.jsx
+++ clientSide/src/App.jsx
@@ -4,16 +4,18 @@ import SignIn from "./components/SignIn"
 import SignUp from "./components/SignUp"
 import About from "./components/About"
 import Profile from "./components/Profile"
+import Header from "./components/Header"
 
 function App() {
 
   return (
     <>
+    <Header/>
       <h1 className="text-red-500">
         Hello world!
       </h1>
       <Routes>
-        <Route path="/" element={<Home/>} ></Route>
+        <Route path="/home" element={<Home/>} ></Route>
         <Route path="/sign-in" element={<SignIn/>} ></Route>
         <Route path="/sign-Up" element={<SignUp/>} ></Route>
         <Route path="/about" element={<About/>} ></Route>
